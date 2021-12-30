18a448c123b88c536960011d7559fcd1805115a1
Mirko Siddi <mirko.siddi@unblur.co>
2021-12-30 11:53:17 +0100

    Error handling

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # mobile-catalogue-server
 API Rest - Postgres DB
+
+
+
+https://documenter.getpostman.com/view/14763811/UVRGEPov
--- index.js
+++ index.js
@@ -13,20 +13,12 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
-
-//CORS
-app.use(
-  cors({
-    credentials: true,
-    origin: [
-      "http://localhost:3000",
-      "http://localhost",
-      "http://127.0.0.1",
-      "http://lmen-confeccion.herokuapp.com",
-      "https://lmen-confeccion.herokuapp.com",
-    ],
-  })
-);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+});
 
 //Import DB Connection and functions
 const db = require('./db/db_queries.js')
