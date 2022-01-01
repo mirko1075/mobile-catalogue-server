eaa33484dc452cbd15ffe130248ae0d9dbf44150
Mirko Siddi <mirko.siddi@unblur.co>
2022-01-01 18:22:02 +0100

    Fixed ssl issue on heroku addign ssl true to connectionstring

--- src/db/db_queries.js
+++ src/db/db_queries.js
@@ -1,7 +1,8 @@
 const Pool = require('pg').Pool
+const Pool = require('pg').Pool
 require("dotenv").config();
 const isProduction = process.env.NODE_ENV === "production";
-const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`;
+const connectionString = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}:ssl=true`;
 const pool = new Pool({
     connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
 });
