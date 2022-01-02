f551dfbc345034231d77a0b3576f748108910ba2
Mirko Siddi <mirko.siddi@unblur.co>
2022-01-02 19:01:24 +0100

    Fixed local DB connection

--- src/conf/ConnectionDb.js
+++ src/conf/ConnectionDb.js
@@ -1,6 +1,11 @@
 const Pool = require('pg').Pool
 class ConnectionPool { 
-  constructor(ssl) { 
+  constructor(user, database, password, port, host, ssl) { 
+    this.user = user;
+    this.database=database;
+    this.password=password;
+    this.port=port;
+    this.host=host;
     this.ssl=ssl
   }
   pool = new Pool({
@@ -10,6 +15,10 @@ class ConnectionPool {
     port: process.env.PG_PORT,
     host: process.env.PG_HOST,
     ssl: this.ssl
+      ?   {
+        rejectUnauthorized:false
+      }
+      : false
   });
 }
 
