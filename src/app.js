6d3c97dfcab0b83df799024e0939ada6e3943c35
Mirko Siddi <mirko.siddi@unblur.co>
2022-01-04 11:19:51 +0100

    Removed headers set on request

--- src/app.js
+++ src/app.js
@@ -15,15 +15,6 @@ if(process.env.NODE_ENV !== 'test') {
 }
 
 app.use(cors());
-
-// MIDDLEWARE
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
-  next();
-});
-
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
